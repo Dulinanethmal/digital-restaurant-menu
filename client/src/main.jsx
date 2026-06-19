8abf47508f4f2eba4bfd1b537f33e8390f3aa191
Dulinanethmal <dulinanethmal1@gmail.com>
2026-06-19 20:52:57 +0530

    update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import Admin from "./Admin";
+
+
+
+import "./index.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Customer Menu */}
+        <Route
+          path="/"
+          element={<App />}
+        />
+
+        {/* Admin Dashboard */}
+        <Route
+          path="/admin"
+          element={<Admin />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
