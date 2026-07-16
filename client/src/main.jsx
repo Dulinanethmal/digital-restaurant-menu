e584197e12de00ba92f456a5adbfca15d3f94da8
Dulinanethmal <dulinanethmal1@gmail.com>
2026-07-16 20:14:16 +0530

    update main.jsx with new application configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,38 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import App from "./App.jsx"; // Imports your router setup
 
-import App from "./App";
-import Admin from "./Admin";
+// Import your global CSS (Make sure this file exists, or remove this line if you only use LandingPage.css)
+import "./index.css"; 
 
-
-
-import "./index.css";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-
-        {/* Customer Menu */}
-        <Route
-          path="/"
-          element={<App />}
-        />
-
-        {/* Admin Dashboard */}
-        <Route
-          path="/admin"
-          element={<Admin />}
-        />
-
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
